ddcd32582822dd1e0da3352d19df9aaaefef6bd5
corneliusroot <geocrasher@gmail.com>
2018-07-10 16:51:23 -0700

    Create script_template.sh

---
+++ script_template.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+######
+#    Scritpname
+#    Created by Awesome Person
+#    MIT License. Yay!
+######
+
+
+bashfunction(){
+		do-bashy-thing-here
+}
+
+		
+if [ "$1" == "" ]; then
+cat <<_EOF_
+##############################################################################
+# Scriptname/ver
+#
+#Script does scripty things
+#
+# Example:
+
+script.sh abcd
+or
+script.sh zyxw
+##############################################################################
+_EOF_
+
+  exit 1
+		else
+
+input=$1
+		bashfunction()
+fi
